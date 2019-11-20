d3d337e849130818081dfbd90040d558f7ae4d33
MIS839 <974150818@qq.com>
2019-11-20 08:52:47 +0800

    helloworld

--- src/com/chinasofti/test/test2.java
+++ src/com/chinasofti/test/test2.java
@@ -1,10 +1,9 @@
 package com.chinasofti.test;
 
-public class test {
+public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("helloworld");
 		System.out.println("hello");
 
 	}
